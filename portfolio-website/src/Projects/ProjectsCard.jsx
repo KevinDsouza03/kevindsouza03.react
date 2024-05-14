34e35c14a8daf6345817f0eba18c837dcd16aaf2
Kevin <kevindsouza476@gmail.com>
2024-05-14 19:38:18 -0400

    Centering text on project divs. fix later

--- portfolio-website/src/Projects/ProjectsCard.jsx
+++ portfolio-website/src/Projects/ProjectsCard.jsx
@@ -4,7 +4,7 @@ import RenderprojectDesc from './RenderprojectDesc.jsx'
 
 export default function ProjectsCard({image, name, desc}) { //Image is the path to my image, name is str, desc is
     return (
-        <div className="grid m-10 grid-cols-2">
+        <div className="flex m-10 grid-cols-2 items-center">
             <RenderprojectImage 
                 image = {image}
                 name = {name}
