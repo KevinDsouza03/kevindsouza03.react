a6745783258fa5f6b9ea41842a1ff6e3f93201bc
KevinDsouza03 <dsouzakev467@gmail.com>
2024-05-29 21:09:42 -0400

    Styling of skills and put logos

--- portfolio-website/src/Skills/Skillscard.jsx
+++ portfolio-website/src/Skills/Skillscard.jsx
@@ -1,13 +1,13 @@
 export default function Skillscard({name, images}) {
     return(
-        <div className="bg-[#64748b] space-x-2 m-10 items-center">
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <div class="px-6 py-4">
-                    <p className="text-white-700 text-base">
+        <div className="bg-[#64748b] space-x-2 m-10 items-center border-dashed border-2 ">
+            <div className="max-w-sm overflow-hidden shadow-lg size-64">
+                <div class="px-6 py-10 ">
+                    <p className="text-white-700 text-base border-b-2 ">
                         {name}
                     </p>
                 </div>
-                <div class="px-6">
+                <div class="px-6 ">
                     {images.map((url, index) => (
                         <img key={index} src={url} />
                     ))}
