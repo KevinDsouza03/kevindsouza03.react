a177688dc8df318f78f07c9c32f9fdbf0870bdc2
KevinDsouza03 <dsouzakev467@gmail.com>
2024-06-03 21:03:21 -0400

    redoing dropdown menu

--- portfolio-website/src/Navbar/Navbar.jsx
+++ portfolio-website/src/Navbar/Navbar.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import './Navbar.css'
-import Menu from './Menu.jsx'
 export default function Navbar() {
     return (
         <>
         <div className="fixed top-10 left-10">
             <p>Kevin Dsouza, Software Engineer</p>
-            <Menu />
+            
         </div>
         </>
     )
