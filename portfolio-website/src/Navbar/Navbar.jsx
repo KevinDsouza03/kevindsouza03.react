2939074310ec47d7ab2ccdfce41d092cca7dc723
KevinDsouza03 <dsouzakev467@gmail.com>
2024-06-04 20:45:11 -0400

    Navbar/Menu styling. Also buttons added below aboutme for socials. More needed, and further styling

--- portfolio-website/src/Navbar/Navbar.jsx
+++ portfolio-website/src/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/react'
 export default function Navbar() {
     return (
-        <>
-        <div className="fixed top-10 left-10">
-            <p>Kevin Dsouza, Software Engineer</p>
-            
-        </div>
-        </>
+        <Menu as="div" className="fixed top-10 left-10 data-[open]:overscroll-contain">
+            <MenuButton>Kevin Dsouza, Software Engineer</MenuButton>
+            <MenuItems className="p-1">
+                <MenuItem>
+                    <a href="https://www.linkedin.com/in/kevindsouza03/">
+                        <img className="size-7" src="../../assets/linkedin.png"/>
+                    </a>
+                </MenuItem>
+                <MenuItem>
+                    <a href="https://github.com/KevinDsouza03">
+                        <img className="size-7" src="../../assets/github.png"/>
+                    </a>
+                </MenuItem>
+            </MenuItems>
+        </Menu>
     )
 
 }
