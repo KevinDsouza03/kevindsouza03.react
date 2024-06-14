93806087bef61d73b1400326cb9a3fb1bcc39eb3
KevinDsouza03 <dsouzakev467@gmail.com>
2024-06-13 22:33:42 -0400

    e

--- app/Aboutme/AboutmeImage.jsx
+++ app/Aboutme/AboutmeImage.jsx
@@ -15,6 +15,11 @@ export default function AboutmeImage() {
                             <img className="size-10" src="github.png"/>
                         </a>
                 </Button>
+                <Button className="rounded p-2 text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700 mx-2 mt-2 ">
+                    <a href="https://github.com/KevinDsouza03">
+                            <img className="size-10" src="github.png"/>
+                        </a>
+                </Button>
             </div>
         </div>
     )
