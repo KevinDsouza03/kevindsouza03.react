ea4963f72f4543d0c9f56b6273c02ae7d1fa00e2
KevinDsouza03 <dsouzakev467@gmail.com>
2024-06-23 12:25:28 -0400

    centered text. looking into deploying

--- app/Projects/RenderprojectImage.jsx
+++ app/Projects/RenderprojectImage.jsx
@@ -2,7 +2,7 @@
 export default function RenderprojectImage({name,image}) {
     return(
         <div className="w-full">
-            <p className="font-bold text-4xl m-5">{name}</p>
+            <p className="font-bold text-4xl m-5 text-center">{name}</p>
             <img className="w-full object-cover" src={image}/>
         </div>
 
