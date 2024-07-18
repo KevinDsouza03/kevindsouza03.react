e212f870c0ccdb34077ec076ddad98a9788da63e
Kevin Dsouza <kevin.dsouza@cix.csi.cuny.edu>
2024-07-18 14:52:25 -0400

    fixed pdf rendering. attemping build again

--- app/CV/page.jsx
+++ app/CV/page.jsx
@@ -2,19 +2,15 @@
 
 import Navbar from "../Navbar/Navbar.jsx";
 import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url,
-).toString();
 
 export default function CV() {
   return (
     <div className="bg-[#343434] p-5">
-      <div className="bg-[#1A2421] bg-opacity-30 p-20">
+      <div className="bg-[#1A2421] bg-opacity-30 p-20 flex h-screen">
         <Navbar />
-        <Document file="Kevin_Dsouza_Resume.pdf">
-          <Page pageNumber={1} width={900} />
-        </Document>
+        <div class="container ">
+          <iframe class="w-full h-full"src='Kevin_Dsouza_Resume.pdf' />
+        </div>
       </div>
     </div>
   );
