a7d14165734b716c9dcfff557a65cd5eed2a43d4
Kevin Dsouza <kevin.dsouza@cix.csi.cuny.edu>
2024-07-23 12:01:12 -0400

    Image styling

--- app/Projects/ProjectsCard.jsx
+++ app/Projects/ProjectsCard.jsx
@@ -4,16 +4,16 @@ import RenderprojectDesc from "./RenderprojectDesc.jsx";
 export default function Projectscard({ image, name, desc, thumbnail, link }) {
   //Image is the path to my image, name is str, desc is
   return (
-    <div className="flex flex-col gap-4">
-    <div className="hover:rounded-lg text-white cursor-pointer flex m-10 items-center gap-40 border-solid border-b-2 p-5">
-      <RenderprojectImage image={image} name={name} />
-      <RenderprojectDesc
-        name={name}
-        desc={desc}
-        thumbnail={thumbnail}
-        link={link}
-      />
-    </div>
+    <div className="flex gap-4">
+      <div className="hover:rounded-lg text-white cursor-pointer flex m-10 items-center gap-40 border-solid border-b-2 p-5">
+        <RenderprojectImage image={image} name={name} />
+        <RenderprojectDesc
+          name={name}
+          desc={desc}
+          thumbnail={thumbnail}
+          link={link}
+        />
+      </div>
     </div>
   );
 }
