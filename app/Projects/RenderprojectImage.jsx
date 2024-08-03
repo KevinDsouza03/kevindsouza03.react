3f3fccc15d69f78b2edd3009f0f4295ece2e4f35
Kevin Dsouza <kevin.dsouza@cix.csi.cuny.edu>
2024-08-03 13:10:08 -0400

    e

--- app/Projects/RenderprojectImage.jsx
+++ app/Projects/RenderprojectImage.jsx
@@ -2,7 +2,7 @@ export default function RenderprojectImage({ name, image }) {
   return (
     <div className="w-full ">
       <p className="font-bold text-4xl m-5 text-center">{name}</p>
-      <img className="w-auto max-w-lg self-center mx-auto  object-contain h-[500px]" src={image} />
+      <img className=" self-center mx-auto  object-contain h-[500px] w-full" src={image} />
     </div>
   );
 }
