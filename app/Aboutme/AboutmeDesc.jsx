4f8f4d2b14884edbd2b0df8a73c8e487ebf583ad
Kevin <kevindsouza476@gmail.com>
2024-08-14 09:20:24 -0400

    adding fonts

--- app/Aboutme/AboutmeDesc.jsx
+++ app/Aboutme/AboutmeDesc.jsx
@@ -2,13 +2,13 @@ export default function AboutmeDesc() {
   return (
     <div className="text-center">
       <div className="text-4xl">
-        <h1 className="">
+        <h1 className="font-libre font-thin">
           Hey, my name is 
           <p>Kevin</p>
         </h1>
       </div>
       <div className="">
-        <p className="text-2xl">
+        <p className="text-1xl">
           A software engineer who loves optimizing things and finding ways to improve.
         </p>
       </div>
