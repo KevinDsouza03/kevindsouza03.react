2bdbc06e374983ab0eb44ea585144405dbf07100
Kevin Dsouza <kevin.dsouza@cix.csi.cuny.edu>
2024-08-26 12:30:08 -0400

    mq revisions

--- app/Skills/Skills.jsx
+++ app/Skills/Skills.jsx
@@ -27,7 +27,7 @@ const libraries = [
 
 export default function Skills() {
   return (
-    <div className="flex items-stretch ">
+    <div className="flex items-stretch">
       <Skillscard name="Languages" images={languages} />
       <Skillscard name="Frameworks" images={frameworks} />
       <Skillscard name="Dev Tools" images={devTools} />
