f2042e686d4d54e0fa9a632618d4038cd82c1f2e
Kevin <kevindsouza476@gmail.com>
2024-08-28 19:34:45 -0400

    Adding more badges

--- app/Skills/Skills.jsx
+++ app/Skills/Skills.jsx
@@ -6,16 +6,21 @@ const languages = [
   "https://img.shields.io/badge/JavaScript-yellow?style=for-the-badge&logo=javascript&labelColor=black",
   "https://img.shields.io/badge/HTML5-white?style=for-the-badge&logo=html5",
   "https://img.shields.io/badge/SQL-%23ffd7b5?style=for-the-badge&logo=mysql",
+  "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white&logoWidth=20"
 ];
 
 const frameworks = [
   "https://img.shields.io/badge/react-blue?style=for-the-badge&logo=react",
   "https://img.shields.io/badge/scikit-learn-orange?style=for-the-badge&labelColor=blue",
+  "https://img.shields.io/badge/NextJS-000000?style=for-the-badge&logo=next.js&logoColor=white",
+  "https://img.shields.io/badge/TailwindCSS-38B2AC?style=for-the-badge&logo=tailwindcss&logoColor=white"
 ];
 
 const devTools = [
   "https://img.shields.io/badge/git-gray?style=for-the-badge&logo=git",
   "https://img.shields.io/badge/VSCode-%23007ACC?style=for-the-badge&logo=visualstudio",
+  "https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white",
+  "https://img.shields.io/badge/Kali_Linux-268BEE?style=for-the-badge&logo=kalilinux&logoColor=white"
 ];
 
 const libraries = [
