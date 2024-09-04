1f523a2392710c8ad5ee5a20282d8f950e03c877
Kevin <kevindsouza476@gmail.com>
2024-09-04 19:58:45 -0400

    mq on aboutmedsc

--- app/Aboutme/AboutmeDesc.jsx
+++ app/Aboutme/AboutmeDesc.jsx
@@ -1,14 +1,14 @@
 export default function AboutmeDesc() {
   return (
     <div className="text-center sm:text-left pr-2 ">
-      <div className="text-4xl">
+      <div className="text-4xl sm:text-2xl">
         <h1 className="">
           Hey, my name is 
           <p>Kevin</p>
         </h1>
       </div>
       <div className="">
-        <p className="text-2xl font-libre ">
+        <p className="text-2xl font-libre sm:text-sm">
           A software engineer who loves optimizing things and finding ways to improve.
         </p>
       </div>
