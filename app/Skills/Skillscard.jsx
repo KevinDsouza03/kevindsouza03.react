d8a43a1e5b841c2cbef654f2c1f2fcb638cd50cf
Kevin <kevindsouza476@gmail.com>
2024-09-18 20:13:14 -0400

    schemes

--- app/Skills/Skillscard.jsx
+++ app/Skills/Skillscard.jsx
@@ -1,8 +1,8 @@
 export default function Skillscard({ name, images }) {
   return (
     <div className="bg-light-mint rounded-lg shadow-md overflow-hidden">
-      <div className="px-6 py-4 bg-mint">
-        <h3 className="text-lg sm:text-xl font-semibold text-center text-gray-800">{name}</h3>
+      <div className="px-6 py-4 bg-olive">
+        <h3 className="text-lg sm:text-xl font-semibold text-center text-light-mint">{name}</h3>
       </div>
       <div className="p-4 grid grid-cols-2 sm:grid-cols-3 gap-2">
         {images.map((url, index) => (
